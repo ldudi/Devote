e139e7a36ec7f57999d60618df332f209f7708f1
Labhesh <labhesh.dudi@gmail.com>
2025-12-26 21:54:25 +0530

    custom header and dark mode

--- Devote/View/NewTaskItemView.swift
+++ Devote/View/NewTaskItemView.swift
@@ -11,6 +11,7 @@ struct NewTaskItemView: View {
     
     //: MARK: - PROPERTIES
     
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @Environment(\.managedObjectContext) private var viewContext
     @State private var task: String = ""
     @Binding var isShowing: Bool
@@ -52,7 +53,7 @@ struct NewTaskItemView: View {
                     .font(.system(size: 24, weight: .bold, design: .rounded))
                     .padding()
                     .background(
-                        Color(UIColor.systemGray6)
+                        isDarkMode ? Color(.tertiarySystemBackground) : Color(.secondarySystemBackground)
                     )
                     .cornerRadius(10)
                 
@@ -74,7 +75,7 @@ struct NewTaskItemView: View {
             .padding(.horizontal)
             .padding(.vertical, 20)
             .background(
-                Color(.white)
+                isDarkMode ? Color(UIColor.secondarySystemBackground) : Color.white
             )
             .cornerRadius(16)
             .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.5), radius: 24)
