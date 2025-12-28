f8154e2adc56dd269b0b35bb1e35c6434f387cae
Labhesh <labhesh.dudi@gmail.com>
2025-12-28 17:52:10 +0530

    custom visual effects

--- Devote/View/BlankView.swift
+++ Devote/View/BlankView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct BlankView: View {
+    // MARK: - PROPERTIES
+    
+    var backgroundColor: Color
+    var backgroundOpacity: Double
+    
+    // MARK: - BODY
+    
     var body: some View {
         VStack {
             Spacer()
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-        .background(Color.black)
-        .opacity(0.5)
+        .background(backgroundColor)
+        .opacity(backgroundOpacity)
+        .blendMode(.overlay)
         .edgesIgnoringSafeArea(.all)
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
-    BlankView()
+    BlankView(backgroundColor: .black, backgroundOpacity: 0.3)
+        .background(BackgroundImageView())
+        .background(backgroundGradient.ignoresSafeArea(.all))
 }
