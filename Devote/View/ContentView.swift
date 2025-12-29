9b9aa6757cefcd5f7ad34e00e1e61879d8b1c539
Labhesh <labhesh.dudi@gmail.com>
2025-12-29 15:04:53 +0530

    sound files

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -66,6 +66,8 @@ struct ContentView: View {
                         // APPEARANCE BUTTON
                         Button(action: {
                             isDarkMode.toggle()
+                            playSound(sound: "sound-tap", type: "mp3")
+                            feedback.notificationOccurred(.success)
                         }) {
                             Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
                                 .resizable()
@@ -83,6 +85,8 @@ struct ContentView: View {
                     
                     Button(action: {
                         showNewTaskItem = true
+                        playSound(sound: "sound-ding", type: "mp3")
+                        feedback.notificationOccurred(.success)
                     }, label: {
                         Image(systemName: "plus.circle")
                             .font(.system(size: 30, weight: .semibold, design: .rounded))
